c60877131e42cfe50f82f074d42a3074146e870a
felwoord <felwoord@hotmail.com>
2018-07-01 13:47:18 -0300

    8.0
    -ShopMenu completed

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,16 +7,33 @@ using UnityEngine.UI;
 public class MenuController : MonoBehaviour {
     public GameObject mainCanvas, shopCanvas, settingsCanvas;
     private Text totalCoinsTxt;
-    private float totalCoins;
+    private int totalCoins;
 
-    // Use this for initialization
+    public GameObject[] skinsGO;
+    private int[] skins;
+    private int[] price;
+    private int skinsCount;
+    private int currentSkin;
+
+
     void Start() {
+        skinsCount = skinsGO.Length;
+        skins = new int[skinsCount];
+        price = new int[skinsCount];
+
+        price[0] = 100;
+        price[1] = 200;
+        price[2] = 300;
+        price[3] = 999999999;
+        price[4] = 999999999;
+
+        PlayerPrefs.SetInt("Skin0", 1);
+
         GameObjectFind();
         GetPlayerPrefs();
         Inicialization();
     }
 
-    // Update is called once per frame
     void Update() {
 
     }
@@ -27,15 +44,35 @@ public class MenuController : MonoBehaviour {
     }
     private void GetPlayerPrefs()
     {
-        totalCoins = PlayerPrefs.GetInt("TotalCoins");
+        totalCoins = PlayerPrefs.GetInt("TotalCoins", 0);
+        totalCoins = 200;
+        currentSkin = PlayerPrefs.GetInt("CurrentSkin", 0);
+        for (int i = 0; i < skinsCount; i++)
+        {
+            skins[i] = PlayerPrefs.GetInt("Skin" + i, 0);
+            if (skins[i] == 0)
+            {
+                skinsGO[i].transform.Find("Price").GetComponent<Text>().text = price[i].ToString();
+            }
+            else
+            {
+                if(currentSkin == i)
+                {
+                    skinsGO[i].transform.Find("Price").GetComponent<Text>().text = "Selected";
+                }
+                else
+                {
+                    skinsGO[i].transform.Find("Price").GetComponent<Text>().text = "Select";
+                }
+
+                skinsGO[i].transform.Find("Price").transform.Find("Image").GetComponent<Image>().enabled = false;
+            }
+        }
     }
     private void Inicialization()
     {
         totalCoinsTxt.text = totalCoins.ToString();
     }
-
-  
-
     public void ChangeCanvas(int aux)
     {
         switch (aux)
@@ -63,5 +100,37 @@ public class MenuController : MonoBehaviour {
     public void PlayButton()
     {
         SceneManager.LoadScene("GameScene");
+    }
+    public void BuySelectSkin(int skinNum)
+    {
+        if (skins[skinNum] == 0) //se nao tiver a skin
+        {
+            if (totalCoins >= price[skinNum])
+            {
+                totalCoins -= price[skinNum];
+                totalCoinsTxt.text = totalCoins.ToString();
+                PlayerPrefs.SetInt("TotalCoins", totalCoins);
+                skins[skinNum] = 1;
+                PlayerPrefs.SetInt("Skin" + skinNum, 1);
+                skinsGO[skinNum].transform.Find("Price").transform.Find("Image").GetComponent<Image>().enabled = false;
+                skinsGO[currentSkin].transform.Find("Price").GetComponent<Text>().text = "Select";
+                currentSkin = skinNum;
+                skinsGO[currentSkin].transform.Find("Price").GetComponent<Text>().text = "Selected";
+
+                PlayerPrefs.SetInt("CurrentSkin", currentSkin);
+                PlayerPrefs.Save();
+            }
+        }
+        else
+        {
+            skinsGO[currentSkin].transform.Find("Price").GetComponent<Text>().text = "Select";
+            currentSkin = skinNum;
+            skinsGO[currentSkin].transform.Find("Price").GetComponent<Text>().text = "Selected";
+
+            PlayerPrefs.SetInt("CurrentSkin", currentSkin);
+            PlayerPrefs.Save();
+        }
+    
+
     }
 }
