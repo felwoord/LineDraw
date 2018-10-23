f2047eb60e65d92b95dfec7a977d130778e85c25
felwoord <felwoord@hotmail.com>
2018-10-23 18:17:27 -0300

    53.0
    -Novos Sprites do menu add

---
+++ Assets/Scripts/DiamondFlare.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiamondFlare : MonoBehaviour {
+    private float alphaColor;
+
+    private bool up = true;
+
+    private Color auxColor;
+    private Color col;
+    private float counter = 0;
+	// Use this for initialization
+	void Start () {
+        col = GetComponent<Image>().color;
+        auxColor = col;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        counter += Time.deltaTime;
+
+        if(up)
+        {
+            alphaColor = Mathf.Lerp(alphaColor, 1, 0.05f);
+            auxColor.a = alphaColor;
+            gameObject.GetComponent<Image>().color = auxColor;
+            if (alphaColor > 0.95f)
+            {
+                up = false;
+            }
+        }
+        else
+        {
+            alphaColor = Mathf.Lerp(alphaColor, 0, 0.05f);
+            auxColor.a = alphaColor;
+            gameObject.GetComponent<Image>().color = auxColor;
+
+            if (alphaColor < 0.05f)
+            {
+                up = true;
+            }
+        }
+
+        transform.Rotate(Vector3.forward * (25 * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/DotAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotAnimation : MonoBehaviour {
+    private float yPosO, yPosF;
+    private float counter = 0;
+    private RectTransform rectTrans;
+
+	// Use this for initialization
+	void Start () {
+        rectTrans = GetComponent<RectTransform>();
+        yPosO = rectTrans.transform.position.y;
+        yPosF = yPosO + 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter += Time.deltaTime;
+
+        if(counter < 0.5f)
+        {
+            transform.position = new Vector3(transform.position.x, Mathf.Lerp(rectTrans. transform.position.y, yPosF, 0.2f), transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, Mathf.Lerp(rectTrans.transform.position.y, yPosO, 0.2f), transform.position.z);
+        }
+
+        if(counter > 1)
+        {
+            counter = 0;
+        }
+	}
+}
