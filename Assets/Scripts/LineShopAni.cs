6c41245cd5205808d7858300b7a61201c5c1d365
felwoord <felwoord@hotmail.com>
2019-01-21 17:56:22 -0200

    65.0

---
+++ Assets/Scripts/LineShopAni.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineShopAni : MonoBehaviour
+{
+    private Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = gameObject.GetComponent<Image>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(img.fillAmount >= 0.99f)
+        {
+            img.fillAmount = 0;
+        }
+        img.fillAmount += Time.deltaTime * 1.5f;
+    }
+}
