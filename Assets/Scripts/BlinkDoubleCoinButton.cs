f266897cb7a093f7d9ef5a12989c72c5915e9b82
felwoord <felwoord@hotmail.com>
2019-03-05 02:15:17 -0300

    83.2
    -efeito blinking DoubleCoin Reward no final da run add

---
+++ Assets/Scripts/BlinkDoubleCoinButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkDoubleCoinButton : MonoBehaviour
+{
+
+    public Text doubleCoinText;
+    private float counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!gameObject.GetComponent<Button>().interactable)
+        {
+            gameObject.GetComponent<BlinkDoubleCoinButton>().enabled = false;
+        }
+        else
+        {
+            counter = 0;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if(counter < 0.2f)
+        {
+            doubleCoinText.enabled = false;
+        }
+        else
+        {
+            doubleCoinText.enabled = true;
+        }
+
+        if(counter > 0.4f)
+        {
+            counter = 0;
+        }
+    }
+}
